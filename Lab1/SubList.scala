e548fb70d8a1d56cdf03e7404071410a6a9b00a7
Daniel Filipe Nunes Silva <danielementary@gmail.com>
2020-09-25 12:03:19 +0200

    add header with names

--- Lab1/SubList.scala
+++ Lab1/SubList.scala
@@ -1,3 +1,8 @@
+//Labs 01 Formal verification (CS-550, EPFL)
+//
+//Samuel Chassot 270955
+//Daniel Filipe Nunes Silva 275197
+
 import stainless.collection._
 import stainless.lang._
 import stainless.annotation._
