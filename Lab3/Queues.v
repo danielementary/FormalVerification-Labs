718004c21dfe9b387b1eace7408062370e91508d
Samuel Chassot <samuel.chassot@epfl.ch>
2020-10-08 16:19:18 +0200

    dequeue non sound prooved

--- Lab3/Queues.v
+++ Lab3/Queues.v
@@ -156,7 +156,7 @@ Proof.
     - unfold toList. unfold fst. unfold snd. simpl. destruct (rev l0) eqn:Eqb.
                                                     * simpl. inversion H. inversion H1. pose proof rev_empty. apply H0. rewrite -> Eqb in H1. simpl in H1. discriminate.
                                                     * inversion H. rewrite Eqb in H1. simpl in H1. discriminate.
-  +
+  + unfold dequeue in H. discriminate.
 Qed.
 
 Lemma dequeue_some_complete:
